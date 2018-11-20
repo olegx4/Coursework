18960a05baa536af41327f16fb86be775eda5f8d
Oleg Shapoval <Olegx4@ukr.net>
2018-11-20 09:21:14 +0200

    Init project

---
+++ build/precache-manifest.21cd449b4b30af613c6076c56b56d951.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bc10ae624f4cc2452f15",
+    "url": "/static/js/main.bc10ae62.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "bc10ae624f4cc2452f15",
+    "url": "/static/css/main.90282f99.chunk.css"
+  },
+  {
+    "revision": "c53a1c972e7c6c0c8c08acfa5534b0ea",
+    "url": "/index.html"
+  }
+];
